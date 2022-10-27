479a62e84ecbda834ebbf740beae5a703130b7fc
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2022-10-27 14:08:43 +0100

    entered id link and name data into links array

--- src/links.js
+++ src/links.js
@@ -1,7 +1,26 @@
 
-const linksObject = {
-    
-}
+const linksObject = [
+    {
+        name:"Twitter Link",
+        link:'https://training.zuri.team/',
+    },
+    {
+        name:"Zuri Team",
+        link:"http://books.zuri.team"
+    },
+    {
+        name:"Zuri Books",
+        link:"https://books.zuri.team/"
+    }, 
+    {
+        name:"Python Books",
+        link:'https://background.zuri.team',
+    },
+    {
+        name:"Design Rules",
+        link:"https://books.zuri.team/design-rules",
+    },
+]
 
 
 export default linksObject
